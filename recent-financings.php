d3bbe94b3849213c7156069b610f63c9d05e139b
Jason Prodonovich <prodonjs@gmail.com>
2015-08-21 16:33:01 -0400

    Added Google Analytics code to all pages

--- recent-financings.php
+++ recent-financings.php
@@ -6,7 +6,18 @@
             About MS&#38;F | Commercial Real Estate Lending
         </title><?php include 'css_js.php'; ?>
         <link href="/css/properties.css" rel="stylesheet" type="text/css">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ 
+  ga('create', 'UA-66442825-1', 'auto');
+  ga('send', 'pageview');
+ 
+</script>
     </head>
+
     <body class="financeBody">
         <div id="wrapper">
             <?php include_once('header.php'); ?>
